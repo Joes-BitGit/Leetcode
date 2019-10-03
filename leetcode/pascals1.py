e9eacd5fa0f9ea3c2af34529e35957a8dcacb34f
Joes-BitGit <32423851+Joes-BitGit@users.noreply.github.com>
2019-10-02 18:56:00 -0700

    Update pascals1.py
    Added comments to the intro to help with functionality

--- leetcode/pascals1.py
+++ leetcode/pascals1.py
@@ -1,3 +1,15 @@
+# Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
+
+# Input: 5
+# Output:
+# [
+#      [1],
+#     [1,1],
+#    [1,2,1],
+#   [1,3,3,1],
+#  [1,4,6,4,1]
+# ]
+
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         if numRows == 0:
