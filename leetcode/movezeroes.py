0a66df0257ddfd33d6f02a517e4b0a8c873a12c1
Joes-BitGit <josephnalmeida@gmail.com>
2020-05-04 21:47:41 -0700

    Added better comments and included time and space complexity

--- leetcode/movezeroes.py
+++ leetcode/movezeroes.py
@@ -1,19 +1,34 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
+        '''
+        Time: O(N), worst case no 0s have to iterate over the entire array
+        Space: O(1)
+        '''
         """
         Do not return anything, modify nums in-place instead.
         """
+        # 2 pointers
+        # non zero ptr
+        n = 0
+        # array ptr
+        a = 0
 
-        # iterates through entire list
-        curr = 0
+        """
+        Mistake was using the i as the other pointer
+        should be its own pointer as in
+        """
 
-        # only iterates through non-zero values
-        ptr = 0
-
+        # iterate over the array
         for i in range(len(nums)):
-            if nums[ptr] != 0:
-                nums[ptr], nums[curr] = nums[curr], nums[ptr]
-                ptr += 1
-                curr += 1
+            # if the non zero ptr is pointing at a non zero number
+            if nums[n] != 0:
+                # swap nonzero and array ptr
+                nums[n], nums[a] = nums[a], nums[n]
+                # inc nonzero ptr
+                n += 1
+                # inc array ptr
+                a += 1
+            # nonzero ptr is pointing at zero number
             else:
-                ptr += 1
+                # inc the non zero ptr
+                n += 1
