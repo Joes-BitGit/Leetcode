b93979a474ce47cb5264bf4d5a8e194359dffa7f
Joes-BitGit <josephnalmeida@gmail.com>
2020-05-06 19:52:12 -0700

    Added an introduction and an example for the daily challenges

--- leetcode/single_num.py
+++ leetcode/single_num.py
@@ -1,3 +1,10 @@
+# Given a non-empty array of integers, every element appears twice except for one.
+# Find that single one.
+
+# EXAMPLE
+# Input: [4,1,2,1,2]
+# Output: 4
+
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         '''
